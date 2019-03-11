ddf871cd204531705ae1992a83aeb690bca2d1f1
Maddie Ijams <maddieijams@gmail.com>
2019-03-11 08:43:08 -0400

    fixed delete

--- src/app/products-service.service.ts
+++ src/app/products-service.service.ts
@@ -4,30 +4,34 @@ import { Product } from './product.model';
 import { Observable, of } from 'rxjs';
 import { AuthenticationService } from './services/authentication.service';
 
-let token = localStorage.getItem('token');
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'Authorization':  token //works if i manually pass in a token
-  })
-};
+
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductsServiceService {
-
+  
   constructor(private http:HttpClient, 
     private authservice:AuthenticationService
     ) { }
-
-  getProducts(){
-    return this.http.get("https://efa-gardenapp-backend.herokuapp.com/api/product");
-  }
-
-
-deleteProduct(id){
+    
+    getProducts(){
+      return this.http.get("https://efa-gardenapp-backend.herokuapp.com/api/product");
+    }
+    
+    
+    deleteProduct(id){
+      // console.log(this.authservice.token)
+      let token = localStorage.getItem('token');
+console.log(token)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization':   token
+    })
+    
+  };
   return this.http.delete(`https://efa-gardenapp-backend.herokuapp.com/api/product/${id}`, httpOptions)
 }
 
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -11,6 +11,8 @@ export class AuthenticationService {
     private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
 
+    token: '';
+
     constructor(private http: HttpClient) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
@@ -26,7 +28,9 @@ export class AuthenticationService {
                 // login successful if there's a jwt token in the response
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    localStorage.setItem('token', user.token);
+                    this.token = user.token;
+                    console.log(this.token)
                     this.currentUserSubject.next(user);
                 }
 
@@ -34,9 +38,9 @@ export class AuthenticationService {
             }));
     }
 
-    getToken(){
-        localStorage.getItem('token')
-    }
+    // getToken(){
+    //     localStorage.getItem('token')
+    // }
 
     logout() {
         // remove user from local storage to log user out
